35bd49d9dbb29b84a5676b87cfd460b87833eac5
Tord Aronsen <Tord.Aronsen@skatteetaten.no>
2023-08-28 15:31:29 +0200

    Calculate work

--- exercise_composite/exercise_models.py
+++ exercise_composite/exercise_models.py
@@ -18,7 +18,11 @@ class Composite(ABC):
         pass
 
     @abstractmethod
-    def get_dependencies(self) -> dict[str, type]:
+    def get_dependencies(self) -> dict[str, str]:
+        pass
+
+    @abstractmethod
+    def calculate_work(self, dependencies: dict[str, float]) -> float:
         pass
 
 
@@ -26,7 +30,7 @@ class Composite(ABC):
 class Movement(Composite):
     name: str
     work_formula: str
-    dependencies: dict[str, type]
+    dependencies: dict[str, str]
     equipment: set[str] = field(default_factory=set)
 
     def add(self, component: "Composite") -> None:
@@ -38,8 +42,15 @@ class Movement(Composite):
     def get_equipment(self) -> set[str]:
         return self.equipment
 
-    def get_dependencies(self) -> dict[str, type]:
+    def get_dependencies(self) -> dict[str, str]:
         return self.dependencies
+
+    def calculate_work(self, dependencies: dict[str, float]) -> float:
+        kcal = 4184.0
+        constants = {"g": 9.80665}
+        local_dict = {**dependencies, **constants}
+        work = float(ne.evaluate(self.work_formula, local_dict=local_dict, global_dict={})) / kcal
+        return work
 
 
 @dataclass
@@ -59,22 +70,31 @@ class Exercise(Composite):
             all_equipment.update(component.get_equipment())
         return all_equipment
 
-    def get_dependencies(self) -> dict[str, type]:
+    def get_dependencies(self) -> dict[str, str]:
         all_dependencies = dict()
         for component in self.components:
             all_dependencies.update(component.get_dependencies())
         return all_dependencies
 
+    def calculate_work(self, dependencies: dict[str, float]) -> float:
+        work = 0.0
+        for component in self.components:
+            work += component.calculate_work(dependencies)
+        return work
+
 
 def main():
     deadlift = Movement(
         name="Deadlift",
-        work_formula="a * b + 4",
-        dependencies={"a": float, "b": float},
+        work_formula="leg_length * mass * g",
+        dependencies={"leg_length": "Length of legs", "mass": "Total mass"},
         equipment={"Barbell", "Locks"},
     )
     front_squat = Movement(
-        name="Front squat", work_formula="b + c", dependencies={"b": float, "c": float}, equipment={"Barbell", "Shoes"}
+        name="Front squat",
+        work_formula="upper_leg_length * mass * g",
+        dependencies={"upper_leg_length": "Length of upper legs", "mass": "Total mass"},
+        equipment={"Barbell", "Shoes"},
     )
     clean = Exercise("Clean")
     clean.add(deadlift)
@@ -82,16 +102,19 @@ def main():
 
     jerk = Movement(
         name="Jerk",
-        work_formula="height * width",
-        dependencies={"height": float, "width": float},
+        work_formula="arm_length * mass * g",
+        dependencies={"arm_length": "Total length of one arm", "mass": "Total mass"},
         equipment={"Barbell", "Belt"},
     )
     clean_jerk = Exercise("Clean & Jerk")
     clean_jerk.add(clean)
     clean_jerk.add(jerk)
+    dependencies = {"leg_length": 0.9, "upper_leg_length": 0.5, "mass": 50, "arm_length": 0.25 + 0.28}
+
     print(clean_jerk)
     print(clean_jerk.get_equipment())
     print(clean_jerk.get_dependencies())
+    print(clean_jerk.calculate_work(dependencies))
 
 
 if __name__ == "__main__":
