0c5b438e888ae82de813485de6fbf12ff75c2451
Tord Aronsen <Tord.Aronsen@skatteetaten.no>
2023-08-30 14:32:55 +0200

    Solution

--- designer_mindset/composition/vehicle_composition_exercise.py
+++ designer_mindset/composition/vehicle_composition_exercise.py
@@ -1,5 +1,11 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from enum import Enum, auto
+from typing import Protocol
+
+
+class Pricing(Protocol):
+    def get_total_price(self):
+        ...
 
 
 class FuelType(Enum):
@@ -15,41 +21,52 @@ class TruckCabStyle(Enum):
 
 
 @dataclass
+class PricePerDay:
+    num_days: int
+    price_per_day: int
+
+    def get_total_price(self) -> int:
+        return self.price_per_day * self.num_days
+
+
+@dataclass
+class PricePerMonth:
+    num_months: int
+    price_per_month: int
+
+    def get_total_price(self) -> int:
+        return self.price_per_month * self.num_months
+
+
+@dataclass
+class PricePerKm:
+    num_km: int
+    price_per_km: int
+
+    def get_total_price(self) -> int:
+        return self.price_per_km * self.num_km
+
+
+@dataclass
 class Vehicle:
     brand: str
     model: str
     color: str
     fuel_type: FuelType
     license_plate: str
-    reserved: bool
+    reserved: bool = False
+    pricing: list[Pricing] = field(default_factory=list)
 
 
 @dataclass
-class VehiclePerDay(Vehicle):
-    price_per_km: int
-    price_per_day: int
-
-
-@dataclass
-class VehiclePerMonth(Vehicle):
-    price_per_month: int
-
-
-@dataclass
-class CarPerDay(VehiclePerDay):
-    number_of_seats: int
-    storage_capacity_litres: int
-
-
-@dataclass
-class CarPerMonth(VehiclePerMonth):
-    number_of_seats: int
-    storage_capacity_litres: int
+class Car(Vehicle):
+    number_of_seats: int = 5
+    storage_capacity_litres: int = 300
 
 
 @dataclass
 class Truck(Vehicle):
-    cab_style: TruckCabStyle
+    cab_style: TruckCabStyle = TruckCabStyle.REGULAR
 
 
 @dataclass
@@ -63,40 +80,34 @@ class Trailer:
 
 def main():
     # Example of a car that you can rent per day
-    ford = CarPerDay(
+    price_per_day = PricePerDay(price_per_day=50, num_days=1)
+    price_per_month = PricePerMonth(price_per_month=1000, num_months=1)
+    price_per_km = PricePerKm(price_per_km=10, num_km=100)
+    ford = Car(
         brand="Ford",
         model="Fiesta",
         color="red",
         fuel_type=FuelType.PETROL,
         license_plate="ABC-123",
         reserved=False,
-        price_per_km=10,
-        price_per_day=50,
+        pricing=[price_per_km, price_per_day, price_per_month],
         number_of_seats=5,
         storage_capacity_litres=300,
     )
     print(ford)
     # Example of a car that you can rent per month
-    tesla = CarPerMonth(
+    tesla = Car(
         brand="Tesla",
         model="Model 3",
         color="black",
         fuel_type=FuelType.ELECTRIC,
         license_plate="DEF-456",
         reserved=False,
-        price_per_month=1000,
+        pricing=[price_per_km, price_per_day, price_per_month],
         number_of_seats=5,
         storage_capacity_litres=300,
     )
     print(tesla)
-
-    # Unfortunately, the current setup makes it impossible to
-    # rent the Tesla per day since it only has a price per month.
-    # The same goes for the Ford, it can only be rented per day and
-    # not per month (at least not easily).
-    # In this exercise, you will have to refactor the code to make
-    # it possible to rent both cars per day and per month, by using
-    # composition over inheritance.
 
 
 if __name__ == "__main__":
