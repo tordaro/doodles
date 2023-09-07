ef53bf17b3eba592b03e10477bb4116a3ea65aed
Tord Aronsen <Tord.Aronsen@skatteetaten.no>
2023-09-07 14:51:23 +0200

    Solution

--- designer_mindset/composition/video_storage_models.py
+++ designer_mindset/composition/video_storage_models.py
@@ -1,25 +1,33 @@
 from cloudengine import CloudProvider
 from cloudengine.google import GoogleAuth
+from dataclasses import dataclass
+
+REGION = "eu-west-1c"
+BUCKET = "video-backup.arjancodes.com"
 
 
-class ACCloud(CloudProvider):
-    def __init__(self, bucket_name: str, region: str) -> None:
-        authentication = GoogleAuth("service_key.json")
-        super().__init__(
-            region=region,
-            http_auth=authentication,
-            secure=True,
-        )
-        self.bucket_name = bucket_name
+@dataclass
+class ACCloud:
+    cloud_provider: CloudProvider
+    bucket_name: str
 
     def find_files(self, query: str, max_result: int) -> list[str]:
-        response = self.filter_by_query(bucket=self.bucket_name, query=query, max=max_result)
+        response = self.cloud_provider.filter_by_query(bucket=self.bucket_name, query=query, max=max_result)
         return response["result"]["data"][0]
 
 
-class VideoStorage(ACCloud):
-    def __init__(self) -> None:
-        super().__init__(
-            bucket_name="video-backup.arjancodes.com",
-            region="eu-west-1c",
-        )
+def create_cloud_provider(region: str = REGION, bucket_name=BUCKET) -> ACCloud:
+    authentication = GoogleAuth("service_key.json")
+    cloud = CloudProvider(region=region, http_auth=authentication, secure=True)
+    return ACCloud(cloud_provider=cloud, bucket_name=bucket_name)
+
+
+def find_files(
+    cloud_provider: CloudProvider,
+    bucket_name: str,
+    query: str,
+    max_result: int,
+) -> list[str]:
+    """Function to avoid ACCloud entirely."""
+    response = cloud_provider.filter_by_query(bucket=bucket_name, query=query, max=max_result)
+    return response["result"]["data"][0]
