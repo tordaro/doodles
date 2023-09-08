07b6baa281207b51c94566ccdb5f576aa01a097f
Tord Aronsen <Tord.Aronsen@skatteetaten.no>
2023-09-08 12:00:47 +0200

    solution

--- designer_mindset/cohesion/csv_reader.py
+++ designer_mindset/cohesion/csv_reader.py
@@ -3,25 +3,34 @@ from typing import Any
 
 Record = dict[str, Any]
 
+INPUT_FILE = "data.csv"
+OUTPUT_FILE = "processed.csv"
+OUTPUT_HEADER = ["name", "status", "is_active"]
+
+
+def read_csv(filename: str) -> list[Record]:
+    with open(filename) as f:
+        reader = csv.DictReader(f)
+        return list(reader)
+
+
+def process_data(row: Record) -> Record:
+    row_copy = row.copy()
+    row_copy["is_active"] = row_copy["status"] == "active"
+    return row_copy
+
+
+def write_csv(filename: str, processed_data: list[Record], header: list[str]) -> None:
+    with open(filename, "w") as f:
+        writer = csv.DictWriter(f, fieldnames=header)
+        writer.writeheader()
+        writer.writerows(processed_data)
+
 
 def main() -> None:
-    with open("data.csv") as f:
-        reader = csv.DictReader(f)
-        data: list[Record] = list(reader)
-
-        processed_data: list[Record] = []
-        for row in data:
-            row_copy = row.copy()
-            if row_copy["status"] == "active":
-                row_copy["is_active"] = True
-            else:
-                row_copy["is_active"] = False
-            processed_data.append(row_copy)
-
-        with open("processed.csv", "w") as f:
-            writer = csv.DictWriter(f, fieldnames=["name", "status", "is_active"])
-            writer.writeheader()
-            writer.writerows(processed_data)
+    data = read_csv(INPUT_FILE)
+    processed_data = [process_data(row) for row in data]
+    write_csv(OUTPUT_FILE, processed_data, OUTPUT_HEADER)
 
 
 if __name__ == "__main__":
