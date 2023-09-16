81c7bf3f74a4b26758b8e9c6458245f866a06ddb
Tord Aronsen <tordaro@gmail.com>
2023-09-16 14:42:58 +0200

    Change to datetime

--- zaptec/get_prices.py
+++ zaptec/get_prices.py
@@ -2,8 +2,11 @@ import requests
 from datetime import datetime
 
 
-def get_daily_price(year: int, month: int, day, price_area: int, cert_path: str = "") -> requests.Response:
-    url = f"https://www.hvakosterstrommen.no/api/v1/prices/{year}/{month:0>2}-{day:0>2}_NO{price_area}.json"
+def get_daily_price(date: datetime, price_area: int, cert_path: str = "") -> requests.Response:
+    url = (
+        "https://www.hvakosterstrommen.no/api/v1/prices/"
+        + f"{date.year}/{date.month:0>2}-{date.day:0>2}_NO{price_area}.json"
+    )
     response = requests.get(url, verify=cert_path)
     return response
 
@@ -13,7 +16,7 @@ def main():
     price_area = 4
     cert_path = "hvakosterstrommen-no-chain.pem"
 
-    response = get_daily_price(now.year, now.month, now.day, price_area, cert_path=cert_path)
+    response = get_daily_price(now, price_area, cert_path=cert_path)
 
     if response.status_code == 200:
         print(f"Prices for {now.date()}: ")
