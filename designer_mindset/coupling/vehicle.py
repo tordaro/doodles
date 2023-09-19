0ac85eb28603ac98bf201ff4ac971d98ec9ffd83
Tord Aronsen <Tord.Aronsen@skatteetaten.no>
2023-09-19 11:07:23 +0200

    Solution

--- designer_mindset/coupling/vehicle.py
+++ designer_mindset/coupling/vehicle.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Iterable
 
 
 @dataclass
@@ -9,6 +10,11 @@ class VehicleData:
     price_per_day: int
     price_per_km: int
 
+    def compute_rental_cost(self, days: int, km: int) -> int:
+        """Computes the rental cost for a vehicle."""
+        paid_kms = max(km - 100, 0)
+        return self.price_per_km * paid_kms + self.price_per_day * days
+
 
 VEHICLE_DATA = {
     "vw": VehicleData(brand="vw", price_per_km=30, price_per_day=6000),
@@ -17,13 +23,11 @@ VEHICLE_DATA = {
 }
 
 
-def read_vehicle_type() -> str:
+def read_vehicle_type(vehicle_data: Iterable[str]) -> str:
     """Reads the vehicle type from the user."""
     vehicle_type = ""
-    while vehicle_type not in VEHICLE_DATA:
-        vehicle_type = input(
-            f"What type of vehicle would you like to rent ({', '.join(VEHICLE_DATA)})? "
-        )
+    while vehicle_type not in vehicle_data:
+        vehicle_type = input(f"What type of vehicle would you like to rent ({', '.join(vehicle_data)})? ")
     return vehicle_type
 
 
@@ -31,9 +35,7 @@ def read_rent_days() -> int:
     """Reads the number of days from the user."""
     days = 0
     while days < 1:
-        days_str = input(
-            "How many days would you like to rent the vehicle? (enter a positive number) "
-        )
+        days_str = input("How many days would you like to rent the vehicle? (enter a positive number) ")
         try:
             days = int(days_str)
         except ValueError:
@@ -45,9 +47,7 @@ def read_kms_to_drive() -> int:
     """Reads the number of kilometers to drive from the user."""
     km = 0
     while km < 1:
-        km_str = input(
-            "How many kilometers would you like to drive (enter a positive number)? "
-        )
+        km_str = input("How many kilometers would you like to drive (enter a positive number)? ")
         try:
             km = int(km_str)
         except ValueError:
@@ -55,27 +55,14 @@ def read_kms_to_drive() -> int:
     return km
 
 
-def compute_rental_cost(vehicle_type: str, days: int, km: int) -> int:
-    """Computes the rental cost for a vehicle."""
-    vehicle_data = VEHICLE_DATA[vehicle_type]
-    price_per_km = vehicle_data.price_per_km
-    price_per_day = vehicle_data.price_per_day
-    paid_kms = max(km - 100, 0)
-    return price_per_km * paid_kms + price_per_day * days
-
-
 def main():
-
-    vehicle_type = read_vehicle_type()
-
+    vehicle_type = read_vehicle_type(VEHICLE_DATA.keys())
     days = read_rent_days()
-
     km = read_kms_to_drive()
 
-    # compute the final rental price
-    rental_price = compute_rental_cost(vehicle_type, days, km)
+    vehicle = VEHICLE_DATA[vehicle_type]
+    rental_price = vehicle.compute_rental_cost(days, km)
 
-    # print the result
     print(f"The total price of the rental is ${(rental_price / 100):.2f}")
 
 
