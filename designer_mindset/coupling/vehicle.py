e1456e3ee515eca3ed258bd5aecfc43772e16bd8
Tord Aronsen <Tord.Aronsen@skatteetaten.no>
2023-09-19 11:17:00 +0200

    solution improvement

--- designer_mindset/coupling/vehicle.py
+++ designer_mindset/coupling/vehicle.py
@@ -9,18 +9,12 @@ class VehicleData:
     brand: str
     price_per_day: int
     price_per_km: int
+    km_free_limit: int = 100
 
     def compute_rental_cost(self, days: int, km: int) -> int:
         """Computes the rental cost for a vehicle."""
-        paid_kms = max(km - 100, 0)
+        paid_kms = max(km - self.km_free_limit, 0)
         return self.price_per_km * paid_kms + self.price_per_day * days
-
-
-VEHICLE_DATA = {
-    "vw": VehicleData(brand="vw", price_per_km=30, price_per_day=6000),
-    "bmw": VehicleData(brand="bmw", price_per_km=35, price_per_day=8500),
-    "ford": VehicleData(brand="ford", price_per_km=25, price_per_day=12000),
-}
 
 
 def read_vehicle_type(vehicle_data: Iterable[str]) -> str:
@@ -56,11 +50,17 @@ def read_kms_to_drive() -> int:
 
 
 def main():
-    vehicle_type = read_vehicle_type(VEHICLE_DATA.keys())
+    vehicle_data = {
+        "vw": VehicleData(brand="vw", price_per_km=30, price_per_day=6000),
+        "bmw": VehicleData(brand="bmw", price_per_km=35, price_per_day=8500),
+        "ford": VehicleData(brand="ford", price_per_km=25, price_per_day=12000),
+    }
+
+    vehicle_type = read_vehicle_type(vehicle_data.keys())
     days = read_rent_days()
     km = read_kms_to_drive()
 
-    vehicle = VEHICLE_DATA[vehicle_type]
+    vehicle = vehicle_data[vehicle_type]
     rental_price = vehicle.compute_rental_cost(days, km)
 
     print(f"The total price of the rental is ${(rental_price / 100):.2f}")
