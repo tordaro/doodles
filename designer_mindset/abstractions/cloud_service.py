e0323bccbb297c1b154bfd6ea5c6e2ed015b6b6f
Tord Aronsen <Tord.Aronsen@skatteetaten.no>
2023-09-21 11:20:18 +0200

    Solution 1

--- designer_mindset/abstractions/cloud_service.py
+++ designer_mindset/abstractions/cloud_service.py
@@ -1,13 +1,30 @@
 from dataclasses import dataclass
+from typing import Protocol
 
-from .google import GoogleCredentials, GoogleServiceProvider, GoogleStorage
+
+class AuthProvider(Protocol):
+    def retrieve_credentials(self) -> str:
+        ...
+
+
+class ServiceProvider(Protocol):
+    def connect(self, credentials: str) -> None:
+        ...
+
+    def get_context(self) -> str:
+        ...
+
+
+class StoreManager(Protocol):
+    def initialize(self, contect) -> None:
+        ...
 
 
 @dataclass
 class CloudService:
-    auth_provider: GoogleCredentials
-    service: GoogleServiceProvider
-    storage_manager: GoogleStorage
+    auth_provider: AuthProvider
+    service: ServiceProvider
+    storage_manager: StoreManager
 
     def connect(self) -> None:
         print("Connecting to the cloud service.")
