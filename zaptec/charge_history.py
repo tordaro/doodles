e6894bd5e2a12945651ac400b5981be3acd236b1
Tord Aronsen <tordaro@gmail.com>
2023-12-11 16:22:25 +0100

    Use dotenv vars

--- zaptec/charge_history.py
+++ zaptec/charge_history.py
@@ -2,7 +2,9 @@ import os
 import requests
 from datetime import datetime
 from pprint import pprint
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def request_charge_history(
     access_token: str, installation_id: str, from_date: datetime, to_date: datetime
@@ -16,15 +18,27 @@ def request_charge_history(
         "From": from_date.strftime(datetime_format),
         "To": to_date.strftime(datetime_format),
     }
-    headers = {"Authorization": f"Bearer {access_token}"}
+    headers = {"accept": "application/json", "Authorization": f"Bearer {access_token}"}
 
-    response = requests.get(endpoint_url, headers=headers, verify=False, params=params)
+    try:
+        response = requests.get(endpoint_url, headers=headers, params=params)
+        print(response.headers)
+        response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
+    except requests.exceptions.HTTPError as errh:
+        print("Http Error:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        print("Error Connecting:", errc)
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:", errt)
+    except requests.exceptions.RequestException as err:
+        print("OOps: Something Else", err)
+
     return response
 
 
 def main():
     access_token = os.getenv("ZAPTEC_TOKEN")
-    installation_id = "5fdd61e6-989b-4d2e-ab75-68e1ee9141cf"
+    installation_id = os.getenv("INSTALLATION_ID")
     from_date = datetime(year=2023, month=9, day=1)
     to_date = datetime(year=2023, month=9, day=5)
     response = request_charge_history(access_token, installation_id, from_date, to_date)
